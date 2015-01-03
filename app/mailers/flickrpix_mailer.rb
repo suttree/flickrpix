9129ec33870678fdda900e0ec754d006f93e82f0
duncan <duncan@suttree.com>
2015-01-03 16:35:58 +0000

    inlined attachements

--- app/mailers/flickrpix_mailer.rb
+++ app/mailers/flickrpix_mailer.rb
@@ -1,6 +1,12 @@
 class FlickrpixMailer < ActionMailer::Base
+  require 'open-uri'
+
   def send_daily_email(urls)
     @urls = urls
+
+    @urls.each do |url|
+      attachments.inline[ url[:image] ] = open(url[:image]).read
+    end
 
     mail(:to => 'duncan.gough@gmail.com', :from => 'duncan.gough+flickrpix@gmail.com', :subject => "flickrpix for #{Date.today.strftime('%b %d, %Y')}")
   end
