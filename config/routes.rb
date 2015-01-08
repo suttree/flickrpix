dc1db5f503bbd37be243ff16c74ea7aa4d4c7590
duncan <duncan@suttree.com>
2015-01-08 16:46:01 +0000

    oauth

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get '/auth' => 'application#auth'
+  get '/callback' => 'application#callback'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
